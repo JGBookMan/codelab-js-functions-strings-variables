bd4a0efcbb17a69d72d4db8a9813e7bf9a68e991
Kevin <stileske@msu.edu>
2016-08-29 20:21:01 -0400

    added and implemented javascript. Stylesheet has issues with flexboxes

---
+++ script.js
@@ -0,0 +1,6 @@
+var name = prompt("What is your favorite name?");
+var color = prompt("What is your favorite color?");
+var food = prompt("What is your favorite food?");
+document.getElementById("name").innerHTML = name;
+document.getElementById("color").innerHTML = color;
+document.getElementById("food").innerHTML = food;
